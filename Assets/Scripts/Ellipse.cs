a79ec6aa4f82ce57a4fb3c2e3da938ba4b7f3288
Jordan Sefah <js986@njit.edu>
2021-11-30 14:59:14 -0800

    added orbital paths

---
+++ Assets/Scripts/Ellipse.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Ellipse : MonoBehaviour
+{
+
+    LineRenderer lr;
+
+    [Range(3, 64)]
+    public int segments;
+    public float xAxis, zAxis;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        CalculateEllipse();
+    }
+
+    void CalculateEllipse()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+        for (int i = 0; i < segments; i++)
+        {
+            float angle = ((float)i / segments) * 360 * Mathf.Deg2Rad;
+            float x = Mathf.Sin(angle) * xAxis;
+            float z = Mathf.Cos(angle) * zAxis;
+            points[i] = new Vector3(x, 40f, z);
+        }
+        points[segments] = points[0];
+        lr.positionCount = segments + 1;
+        lr.SetPositions(points);
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying)
+            CalculateEllipse();
+    }
+}
