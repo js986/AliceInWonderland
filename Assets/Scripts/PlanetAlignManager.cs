be0109818b312833c40972a3eaf50ccb29cba1cc
ydaiydai <yidaiforgames@gmail.com>
2021-11-30 19:22:14 -0800

    Changed Typo

--- Assets/Scripts/PlanetAlignManager.cs
+++ Assets/Scripts/PlanetAlignManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetAlighManager : MonoBehaviour
+public class PlanetAlignManager : MonoBehaviour
 {
 
     GameObject[] planets;
